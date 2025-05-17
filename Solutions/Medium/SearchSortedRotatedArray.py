9da28dabc32ad50610b16e6a4605aac4ef37ae59
Khoi Duong <khoiduongbusiness@gmail.com>
2025-05-16 20:45:35 -0400

    Add files via upload

---
+++ Solutions/Medium/SearchSortedRotatedArray.py
@@ -0,0 +1,32 @@
+from typing import List
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+        while left < right:
+            mid = (left + right) // 2
+            if nums[mid] > nums[right]:
+                left = mid + 1
+            else:
+                right = mid
+        
+        pivot = left
+
+        def binary_search(l, r):
+            while l <= r:
+                m = (l + r) // 2
+                if nums[m] < target:
+                    l = m + 1
+                elif nums[m] > target:
+                    r = m - 1
+                else:
+                    return m
+
+            return -1
+
+        result = binary_search(0, pivot - 1)
+        if result != -1:
+            return result
+        
+        return binary_search(pivot, len(nums) - 1)
